c1d666e5ac8a7592bfbd91fcbbd60de28b55a1ff
Prateek Sinha <31709506+shekharprateek@users.noreply.github.com>
2019-06-23 11:58:03 -0400

    Add files via upload
    My first Java code

---
+++ Myfirstclass.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int x = 20;
+		int y = 30;
+		int result = x + y;
+		System.out.println("Result =" +result);
+
+	}
+
+}
