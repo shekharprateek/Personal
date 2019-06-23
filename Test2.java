10e22afdd07e0499aff6cfce9d6cc7e362683a2a
Prateek Sinha <31709506+shekharprateek@users.noreply.github.com>
2019-06-23 11:58:41 -0400

    Add files via upload
    If Else Java Code

---
+++ Test2.java
@@ -0,0 +1,26 @@
+package Ifelse;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int x = 50 ;
+		int y = 30;
+		if (x<y) {
+			System.out.println("X is less than Y");
+		}
+		
+		else if(x == y)
+		{System.out.println("X is equal to Y");
+		
+		}
+		else
+		{ 
+			System.out.println("X is greater than Y");
+		}
+		 
+		
+		
+
+	}
+
+}
